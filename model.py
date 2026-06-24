490bb406b3c04ee02bc6c059c391a4dad54d08e1
Kwesi Manu Eghan <115375160+keghan001@users.noreply.github.com>
2026-06-24 13:36:23 +0000

    Modular residual block with shortcut funcs

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ResidualBlock(nn.Module):
@@ -8,15 +9,31 @@ class ResidualBlock(nn.Module):
         
         #Building the residual sequence
         self.conv1 = nn.Conv2d(in_channels, out_channels, 
-                            3, stride, padding=1, bias=False),
+                            3, stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         
         self.conv2 = nn.Conv2d(out_channels, out_channels, 
                             3, stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         
+        self.shortcut = nn.Sequential()
+        self.use_shortcut = stride != 1 or in_channels != out_channels
         
+        if self.use_shortcut:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride, bias=False), nn.BatchNorm2d(out_channels)
+            )
         
     def forward(self, x):
+        out = self.conv2(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
         
-        return x
+        shortcut = self.shortcut(x) if self.use_shortcut else x
+        out_add = out + shortcut
+        out = F.relu(out_add)
+        
+        return out
+    
