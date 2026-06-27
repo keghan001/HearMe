960945591e82d85b7ee98c379bc6ea69cfc667c9
Kwesi Manu Eghan <115375160+keghan001@users.noreply.github.com>
2026-06-27 00:30:51 +0000

    feat(dataset-class): ESC50 Dataset class to handle indexing for training

--- train.py
+++ train.py
@@ -1,4 +1,9 @@
 import modal
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path
+import pandas as pd
+import torchaudio
 
 app = modal.App("hear-me")
 
@@ -6,16 +11,55 @@ image = (modal.Image.debian_slim()
             .pip_install_from_requirements("requirements.txt")
             .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
             .run_commands([
-                "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -0 esc50.zip",
+                "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip",
                 "cd /tmp && unzip esc50.zip",
                 "mkdir -p /opt/esc50-data",
-                "cp -r /tmp/ESC-50-master/* /opt/esc-50-data/",
+                "cp -r /tmp/ESC-50-master/* /opt/esc50-data/",
                 "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
             ])
             .add_local_python_source("model"))
 
 volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
 model_volume = modal.Volume.from_name("esc-model", create_if_missing=True)
+
+class ESC50Dataset(Dataset):
+    """Some Information about ESC50Dataset"""
+    def __init__(self, data_dir, metadata_file, split="train", transform=None):
+        super().__init__()
+        self.data_dir = Path(data_dir)
+        self.metadata = pd.read_csv(metadata_file)
+        self.split = split.lower()
+        self.transform = transform
+        
+        if split == "train":
+            self.metadata = self.metadata[self.metadata['fold'] != 5]
+        else:
+            self.metadata = self.metadata[self.metadata['fold'] == 5]
+            
+        self.classes = sorted(self.metadata["category"].unique())
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+        
+        self.metadata["label"] = self.metadata["category"].map(self.class_to_idx)
+
+    def __len__(self):
+        return len(self.metadata)
+    
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+        audio_path = self.data_dir / "audio" / row["filename"]
+        
+        waveform, sample_rate = torchaudio.load(audio_path)
+        
+        if waveform.shape[0] > 1: # [channel, samples] : [2, 44000] -> [1, 44000] 
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+            
+        if self.transform:
+            spectogram = self.transform(waveform)
+        else:
+            spectogram = waveform
+        
+        return spectogram, row["label"]
+            
 
 
 @app.function(image=image, gpu="A10G", volumes={"/data": volume, "/models": model_volume}, timeout= 60 * 60 * 3)
